a3c40da3a9f9b513c10e8f4dbc6576e073fccb45
Liam Ronald Moore <lmoore90@gmail.com>
2021-07-22 21:35:35 +0200

    lets learn fastapi

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def index():
+    return {"data":{"name":"Liam"}}
+
+
+@app.get("/about")
+def about():
+    return {"data":"woopzaza"}
