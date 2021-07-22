5a29c1981c3907193448ecc0081f89fe796c34a0
Liam Ronald Moore <lmoore90@gmail.com>
2021-07-22 22:09:52 +0200

    1

--- main.py
+++ main.py
@@ -6,9 +6,16 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data":{"name":"Liam"}}
+    return {"data":"blog list"}
 
 
-@app.get("/about")
-def about():
-    return {"data":"woopzaza"}
+@app.get("/blog/{id_}")
+def show(id_: int):
+    # fetch blog with id_ = id_
+    return {"data":id_}
+
+
+@app.get("/blog/{id_}/comments")
+def comments(id_: int):
+    """retch comments of blog with id"""
+    return {"data": [f"comment 1 for {id_}"]}
