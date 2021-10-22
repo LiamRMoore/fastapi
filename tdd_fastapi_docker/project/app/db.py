46a48c0cc29c198ec9a041e64a24a2ee8b463869
Liam Ronald Moore <lmoore90@gmail.com>
2021-10-22 14:34:56 +0200

    add tortoise-only schema generation function in db.py (quick alternative to aerich). implement pydantic model for summary payload

--- tdd_fastapi_docker/project/app/db.py
+++ tdd_fastapi_docker/project/app/db.py
@@ -1,7 +1,12 @@
 import os
+import logging
 
 from fastapi import FastAPI
+from tortoise import Tortoise, run_async
 from tortoise.contrib.fastapi import register_tortoise
+
+
+log = logging.getLogger("uvicorn")
 
 
 TORTOISE_ORM = {
@@ -22,3 +27,24 @@ def init_db(app: FastAPI) -> None:
         generate_schemas=False, # aerich will do this
         add_exception_handlers=True
     )
+
+
+async def generate_schema() -> None:
+    """
+    Alternative schema generation to migration via Aerich (can be slow)
+
+    Just run this file in the container and Tortoise will handle it
+    """
+    log.info("Initializing Tortoise...")
+
+    await Tortoise.init(
+        db_url=os.environ.get("DATABASE_URL"),
+        modules={"models": ["models.tortoise"]}
+    )
+    log.info("Generating database schema via Tortoise...")
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+if __name__ == "__main__":
+    run_async(generate_schema())
